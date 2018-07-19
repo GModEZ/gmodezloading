2c2dfd19472b99761c5a75cdff6f6e0cabbfea58
GModEZ <41447282+GModEZ@users.noreply.github.com>
2018-07-19 14:17:44 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -113,8 +113,9 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{youtube: "https://youtu.be/OwbI9IY9Roo", name: "Ric Flair Drip - 21 Savage"},
+	{youtube: "https://youtu.be/OwbI9IY9Roo", name: "21 Savage - Ric Flair Drip"},
 	{youtube: "https://youtu.be/BSCOPmFY1ys", name: "Young N Fly - Another One"},
+	{youtube: "https://youtu.be/pgN-vvVVxMA", name: "XXXTentacion - Sad"},
 ];
 
 // Random music order?
