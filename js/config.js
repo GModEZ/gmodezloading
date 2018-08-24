a267be1ccf73bd3790eef844b2ae96194735f93d
GModEZ <41447282+GModEZ@users.noreply.github.com>
2018-08-23 21:23:11 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@
 // Leave it blank to use text instead
 // (Put the files in the images folder)
 // (You can also use external URL)
-var l_serverImage = "logo1.png";
+var l_serverImage = "ezservers.png";
 
 // Center logo?
 // 'true' or 'false'
